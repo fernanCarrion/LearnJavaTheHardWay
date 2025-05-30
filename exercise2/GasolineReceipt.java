9751b79b184f8852223d542e7494f5745a575f48
fernanCarrion <fercg2004@icloud.com>
2025-05-30 19:42:48 +0200

    Add files via upload

---
+++ exercise2/GasolineReceipt.java
@@ -0,0 +1,18 @@
+package exercise2;
+
+public class GasolineReceipt {
+	public static void main(String[] Args) {
+		System.out.println("+------------------------+");
+		System.out.println("|                        |");
+		System.out.println("|      CORNER STORE      |");
+		System.out.println("|                        |");
+		System.out.println("|   2025-05-30 19:27PM   |");
+		System.out.println("|                        |");
+		System.out.println("|  Litres:  14.870       |");
+		System.out.println("|  Price/Litre: €1.474   |");
+		System.out.println("|                        |");
+		System.out.println("|  Fuel total: 21.92€    |");
+		System.out.println("|                        |");
+		System.out.println("+------------------------+");
+	}
+}
