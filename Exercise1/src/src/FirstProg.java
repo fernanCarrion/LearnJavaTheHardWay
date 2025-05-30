6e4ba8ae50999e656f3c7cc23380cfcb65d3c1de
fernanCarrion <fercg2004@icloud.com>
2025-05-30 19:13:15 +0200

    Add files via upload

---
+++ Exercise1/src/src/FirstProg.java
@@ -0,0 +1,10 @@
+package src;
+
+import java.time.LocalDate;
+
+public class FirstProg {
+	public static void main( String[] args) {
+		System.out.println( "My name is Fernando and I'm determined to learn to code");
+		System.out.println( "Today's date is " + LocalDate.now());
+	}
+}
