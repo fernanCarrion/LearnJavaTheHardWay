fd691a18a21cb93c97329f898f446d4e0b6f06a0
fernanCarrion <fercg2004@icloud.com>
2025-05-31 12:46:36 +0200

    Add files via upload

---
+++ exercise3/PrintigChoices.java
@@ -0,0 +1,28 @@
+package exercise3;
+
+public class PrintigChoices {
+	public static void main(String[] args) {
+		System.out.println("Alpha");
+		System.out.println("Bravo");
+		
+		System.out.println("Charlie");
+		System.out.println("Delta");
+		System.out.println();
+		
+		System.out.println("Echo");
+		System.out.println("Foxtrot");
+		
+		System.out.println("Golf");
+		System.out.print("Hotel");
+		System.out.println();
+		System.out.println("India");
+		
+		System.out.println();
+		System.out.println("This"+" "+"is"+" a"+" test.");
+	
+		System.out.println("I'm"+" "+"learnig"+" java"+ " the"+" hard"+" way");
+	
+	}
+	
+	
+}
